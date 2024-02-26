018331fb132e0202c1ef6b83fb0aa336ec0406f1
fsgdoterr <fsgdoterr@gmail.com>
2024-02-26 15:50:15 +0200

    Main repository

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
 import { configModuleOptions } from 'src/config/config';
 import { mongooseModuleAsyncOptions } from 'src/config/mongoose';
+import { RepositoryModule } from 'src/services/repositories/repository/repository.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot(configModuleOptions),
         MongooseModule.forRootAsync(mongooseModuleAsyncOptions),
         PassportModule.register({}),
+        RepositoryModule,
     ],
 })
 export class AppModule {}
