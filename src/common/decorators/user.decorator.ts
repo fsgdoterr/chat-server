f7cf66d9ac6bbd7b5be56ed056a6ab45bd36bdba
fsgdoterr <fsgdoterr@gmail.com>
2024-02-27 12:56:28 +0200

    User decorator

---
+++ src/common/decorators/user.decorator.ts
@@ -0,0 +1,11 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+
+export const User = createParamDecorator(
+    (data: string, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+        const user = request.user;
+        if(data) return user[data];
+        return user;
+    }
+)
